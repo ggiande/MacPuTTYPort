ed3980d90073f7b6beaca67cf3030200724cef5f
Giancarlo Garcia Deleon <44848977+ggiande@users.noreply.github.com>
2019-01-14 16:48:31 -0800

    Updated the automatic installation of xQuartz.

--- time.sh
+++ time.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
-#curl -L -O https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.11.dmg
-#sh -c "$(curl -fsSL https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.11.dmg)" Apparently only for github raw
-#open -a "Safari" https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.11.dmg
-#curl --remote-name https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.11.dmg
-#curl -# -O http://www.cyberciti.biz/files/mastering-vi-vim.pdf
-
-
 #Automatic download of xQuartz, a graphical software that will display putty for us.
-
-if open -Ra "xQuartz"; then
-echo "VERIFIED: 'xQuartz' is installed on this machine."
-
+echo ""
+echo ""
+echo "Now checking to see if xQuartz has been previously installed on this machine. If so then xQuartz will be directly downloaded to the desktop for convenience."
+if [ -d /Applications/Utilities/XQuartz.app ]; then
+    echo "VERIFIED: 'xQuartz' is installed on this machine."
 else
-    echo "xQuartz will now be installed in Desktop for convenience."
     cd ~/Desktop/
     curl -L -O https://dl.bintray.com/xquartz/downloads/XQuartz-2.7.11.dmg
+    echo "xQuartz will now be installed in Desktop for convenience."
+    cd ~/Desktop/
+    sudo hdiutil attach XQuartz-2.7.11.dmg
+    echo "Attachhing and Installing. This may take a while!"
+    sudo installer -pkg /Volumes/XQuartz-2.7.11/XQuartz.pkg  -target /
+    sudo hdiutil detach XQuartz-2.7.11.dmg
+    echo "Detaching."
+    echo "xQuartz is now installed or has been previously installed on this machine."
 fi
-echo "Line"
-echo "Line"
-echo "Line"
-echo "Check for Install."
-while [ ! -d /Applications/Utilities/XQuartz.app ];
-do
-    sleep 1;
-done
-    echo "Already Installed"
+echo ""
+echo ""
